3ac037d2113bebbad970ef2fb48bd9105dd45eab
tynt7 <tynt7@my.yorku.ca>
2018-01-15 11:57:25 -0500

    Activity 1 - Push

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -9,6 +9,6 @@ public class ToyAuthoring {
     	    //Lab1 Task
     	    System.out.println("Team 9: push2");
     	    System.out.println("Hello World");
-    	    System.out.println("Team 9 Lab Activity 1");
+    	    System.out.println("Team 9 - Lab Activity 1");
     }
 }
