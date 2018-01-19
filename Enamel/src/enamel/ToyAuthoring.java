ad670d66e23081e4c6227f619932892f63304955
JeremyWinkler <jeremy.winkler1@gmail.com>
2018-01-19 12:14:05 -0500

    Added File Chooser

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -1,14 +1,22 @@
 package enamel;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
 public class ToyAuthoring {
 
-    public static void main(String[] args) { 	  	
-    	    ScenarioParser s = new ScenarioParser(true);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
-    	    
-    	    //Lab1 Task
-    	    System.out.println("Team 9: push2");
-    	    System.out.println("Team 9: push3");
+	public static void main(String[] args) {
+		JButton open = new JButton();
+		JFileChooser fc = new JFileChooser();
+		fc.setCurrentDirectory(new java.io.File("./FactoryScenarios"));
+		fc.setDialogTitle("Please Choose File to Open");
+		fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		if (fc.showOpenDialog(open) == JFileChooser.APPROVE_OPTION) {
+			
+		}
+		ScenarioParser s = new ScenarioParser(true);
+		s.setScenarioFile(fc.getSelectedFile().getPath());
 
-    }
+		
+	}
 }
