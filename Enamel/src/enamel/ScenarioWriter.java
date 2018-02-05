a0b0703d85b5c4ebd251d430c02f0486e7f5c6b4
tynt7 <tynt7@my.yorku.ca>
2018-02-05 11:34:26 -0500

    Created a ScenarioWriter

--- Enamel/src/enamel/ScenarioWriter.java
+++ Enamel/src/enamel/ScenarioWriter.java
@@ -1,5 +1,6 @@
 package enamel;
 
+
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
@@ -9,15 +10,18 @@ public class ScenarioWriter {
 	private String path;
 	private boolean append = false;
 	
+	//constructor for creating a file
 	public ScenarioWriter (String filePath) {
 		path = filePath;
 	}
 	
+	//constructor form editing file
 	public ScenarioWriter (String filePath, boolean appendVal) {
 		path = filePath;
 		append = appendVal;
 	}
 	
+	//write to a document
 	public void write(String text) throws IOException{
 		FileWriter write = new FileWriter(path, append);
 		PrintWriter print = new PrintWriter(write);
