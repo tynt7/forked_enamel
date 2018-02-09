85832972d822c1faf6dc5243f36fc83e64cc6541
tynt7 <tynt7@my.yorku.ca>
2018-02-09 09:58:40 -0500

    Added basic view for Authoring App

--- Enamel/src/enamel/AuthoringViewer.java
+++ Enamel/src/enamel/AuthoringViewer.java
@@ -3,6 +3,13 @@ package enamel;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+import net.miginfocom.swing.MigLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
 
 public class AuthoringViewer extends JFrame {
 	private JLabel prompt;
@@ -23,11 +30,132 @@ public class AuthoringViewer extends JFrame {
 	private JCheckBox b3CB;
 	private JCheckBox b4CB;
 	private JList list;
+	private JTextField textField;
+	private JTextField letterInput;
 	
 	public AuthoringViewer(ActionListener listener) {
 		super("File Creator");
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(23, 150, 130, 26);
+		getContentPane().add(letterInput);
+		textField.setColumns(10);
+		
+		letterInput = new JTextField();
+		letterInput.setBounds(20, 205, 150, 30);
+		getContentPane().add(letterInput);
+		letterInput.setColumns(10);
+		
+		JButton exit = new JButton("X");
+		exit.setBounds(20, 296, 50, 50);
+		getContentPane().add(exit);
+		
+		JButton save = new JButton("S");
+		save.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		save.setBounds(70, 296, 50, 50);
+		getContentPane().add(save);
+		
+		JButton test = new JButton("T");
+		test.setBounds(120, 296, 50, 50);
+		getContentPane().add(test);
+		
+		JButton left = new JButton("<");
+		left.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		left.setBounds(70, 240, 21, 29);
+		getContentPane().add(left);
+		
+		JButton right = new JButton(">");
+		right.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		right.setBounds(103, 240, 21, 29);
+		getContentPane().add(right);
+		
+		JList orderList = new JList();
+		orderList.setBounds(435, 40, 197, 114);
+		getContentPane().add(orderList);
+		
+		JEditorPane promptText = new JEditorPane();
+		promptText.setBounds(183, 40, 240, 114);
+		getContentPane().add(promptText);
+		
+		JLabel lblPrompt = new JLabel("PROMPT");
+		lblPrompt.setBounds(183, 12, 61, 16);
+		getContentPane().add(lblPrompt);
+		
+		JLabel lblCard = new JLabel("ORDER");
+		lblCard.setBounds(435, 12, 61, 16);
+		getContentPane().add(lblCard);
+		
+		JLabel lblAudio = new JLabel("AUDIO");
+		lblAudio.setBounds(183, 166, 61, 16);
+		getContentPane().add(lblAudio);
+		
+		JTextPane audioFile = new JTextPane();
+		audioFile.setText("AudioFileName.mp3");
+		audioFile.setBounds(182, 182, 150, 16);
+		getContentPane().add(audioFile);
+		
+		JButton recButt = new JButton("R");
+		recButt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		recButt.setBounds(333, 182, 30, 16);
+		getContentPane().add(recButt);
+		
+		JButton impButt = new JButton("I");
+		impButt.setBounds(363, 182, 30, 16);
+		getContentPane().add(impButt);
+		
+		JButton delButt = new JButton("D");
+		delButt.setBounds(393, 182, 30, 16);
+		getContentPane().add(delButt);
+		
+		JLabel lblButtons = new JLabel("BUTTONS");
+		lblButtons.setBounds(182, 212, 61, 16);
+		getContentPane().add(lblButtons);
+		
+		JEditorPane bttnText = new JEditorPane();
+		bttnText.setBounds(183, 284, 449, 79);
+		getContentPane().add(bttnText);
+		
+		JPanel bttnList = new JPanel();
+		bttnList.setBounds(182, 230, 450, 42);
+		getContentPane().add(bttnList);
+		bttnList.setLayout(new BoxLayout(bttnList, BoxLayout.X_AXIS));
+		
+		JToggleButton one = new JToggleButton("1");
+		bttnList.add(one);
+		
+		JToggleButton two = new JToggleButton("2");
+		bttnList.add(two);
+		
+		JToggleButton three = new JToggleButton("3");
+		bttnList.add(three);
+		
+		JToggleButton toggleButton = new JToggleButton("4");
+		bttnList.add(toggleButton);
+		
+		JToggleButton toggleButton_1 = new JToggleButton("5");
+		bttnList.add(toggleButton_1);
+		
+		JToggleButton toggleButton_2 = new JToggleButton("6");
+		bttnList.add(toggleButton_2);
+		
+		JLabel currCard = new JLabel("currCard");
+		currCard.setBounds(20, 12, 61, 16);
+		getContentPane().add(currCard);
 		
 		this.makeLables();
 		this.makeTextFields();
@@ -36,24 +164,6 @@ public class AuthoringViewer extends JFrame {
 		this.makeMenu();
 		this.makeJCheckBox();
 		this.makeList();
-		
-		this.add(prompt);
-		this.add(promptTF);
-		this.add(b1L);
-		this.add(b1TF);
-		this.add(b1CB);
-		this.add(b2L);
-		this.add(b2TF);
-		this.add(b2CB);
-		this.add(b3L);
-		this.add(b3TF);
-		this.add(b3CB);
-		this.add(b4L);
-		this.add(b4TF);
-		this.add(b4CB);
-		this.add(rbLetter);
-		this.add(rbPins);
-		this.add(list);
 		
 	}
 
@@ -91,5 +201,4 @@ public class AuthoringViewer extends JFrame {
 		// TODO Auto-generated method stub
 		
 	}
-	
 }
