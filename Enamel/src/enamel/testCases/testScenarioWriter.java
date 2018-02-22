322912a919edf2456539806a011445ccf5f9d9d3
tynt7 <tynt7@my.yorku.ca>
2018-02-22 00:19:28 -0500

    Test Cases, Spelling Issues, Comments

---
+++ Enamel/src/enamel/testCases/testScenarioWriter.java
@@ -0,0 +1,48 @@
+package enamel.testCases;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.*;
+
+import enamel.ScenarioWriter;
+
+public class testScenarioWriter {
+
+	private boolean val = true;
+	private String path;
+	private ScenarioWriter sw;
+
+	@Before
+	public void setUp() throws Exception {
+		path = "./";
+	}
+
+	/*
+	 * Creates a file and tests if it writes correctly
+	 */
+	@Test
+	public void test() throws IOException {
+		sw = new ScenarioWriter(path);
+		sw.write("Hello\n");
+		
+	}
+
+	/*
+	 * Appends the file created in test one and test the contents
+	 */
+	@Test
+	public void test2() throws IOException {
+		sw = new ScenarioWriter(path, true);
+		sw.write("Bye");
+	}
+
+	/*
+	 * 
+	 */
+	@Test
+	public void test3() {
+
+	}
+}
