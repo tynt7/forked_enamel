5387269ab0ffc346deae1fa3c608d369c76dbe7a
NS-01 <nskk01@my.yorku.ca>
2018-02-22 14:07:06 -0500

    TC started

--- Enamel/src/enamel/testCases/testInitialView.java
+++ Enamel/src/enamel/testCases/testInitialView.java
@@ -2,7 +2,9 @@ package enamel.testCases;
 
 import static org.junit.Assert.*;
 
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import enamel.DataButton;
@@ -10,6 +12,25 @@ import enamel.InitialView;
 
 class testInitialView {
 	private InitialView iv;
+	
+	/**
+	 * Setup before class
+	 * @throws Exception
+	 */
+	@BeforeClass
+	public static void setUpClass() throws Exception{
+		
+	}
+	
+	/**
+	 * Tear down class a the end
+	 * @throws Exception
+	 */
+	@AfterClass
+	public static void tearDownClass() throws Exception{
+		
+	}
+	
 	/**
 	 * Initial Setup
 	 * 
