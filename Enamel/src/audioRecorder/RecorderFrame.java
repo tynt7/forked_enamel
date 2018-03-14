b203788b33d9ffcad3f5bcf89f6a85471c2a903f
guru <guru@Krishna>
2018-03-14 08:21:52 -0400

    SR Changes

--- Enamel/src/audioRecorder/RecorderFrame.java
+++ Enamel/src/audioRecorder/RecorderFrame.java
@@ -27,6 +27,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import javax.swing.filechooser.FileFilter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 //import java.text.DateFormat;
 //import java.text.SimpleDateFormat;
 import java.awt.event.ActionEvent;
@@ -515,6 +519,71 @@ public class RecorderFrame {
 					+ "possibilities include: \n Incorrect name of the file, the file not being in the same location "
 					+ "as the project folder, or an attempt to play an unsupported sound file. (only .wav files"
 					+ "are supported at this time)");
+		}
+	}
+	
+	
+	/**
+	 * This class counts record time/play time in the form of HH:mm:ss
+	 * @author www.codejava.net
+	 *
+	 */
+	public class RecordTimer extends Thread {
+		private DateFormat dateFormater = new SimpleDateFormat("HH:mm:ss");	
+		private boolean isRunning = false;
+		private boolean isReset = false;
+		private long startTime;
+		private JLabel labelRecordTime;
+		
+		RecordTimer(JLabel labelRecordTime) {
+			this.labelRecordTime = labelRecordTime;
+		}
+		
+		public void run() {
+			isRunning = true;
+			
+			startTime = System.currentTimeMillis();
+			
+			while (isRunning) {
+				try {
+					Thread.sleep(1000);
+					labelRecordTime.setText("Record Time: " + toTimeString());
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+					if (isReset) {
+						labelRecordTime.setText("Record Time: 00:00:00");
+						isRunning = false;		
+						break;
+					}
+				}
+			}
+		}
+		
+		/**
+		 * Cancel counting record/play time.
+		 */
+		void cancel() {
+			isRunning = false;		
+		}
+		
+		/**
+		 * Reset counting to "00:00:00"
+		 */
+		void reset() {
+			isReset = true;
+			isRunning = false;
+		}
+		
+		/**
+		 * Generate a String for time counter in the format of "HH:mm:ss"
+		 * @return the time counter
+		 */
+		private String toTimeString() {
+			long now = System.currentTimeMillis();
+			Date current = new Date(now - startTime);
+			dateFormater.setTimeZone(TimeZone.getTimeZone("GMT"));
+			String timeCounter = dateFormater.format(current);
+			return timeCounter;
 		}
 	}
 }
